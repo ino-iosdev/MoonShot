309e9843ff343da6cf5d7854524fe68b1877c7e2
ino yang popper <ino.yang.p@gmail.com>
2025-04-08 00:39:40 -0700

    created MoonShot

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Ino Yang Popper on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
