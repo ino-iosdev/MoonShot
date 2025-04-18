58500d0a354548bb613d6761bbba255becf20a72
ino yang popper <ino.yang.p@gmail.com>
2025-04-18 00:15:56 -0700

    updated

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Ino Yang Popper on 4/11/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MoonShot/MissionView.swift
@@ -0,0 +1,52 @@
+//
+//  MissionView.swift
+//  MoonShot
+//
+//  Created by Ino Yang Popper on 4/17/25.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let crew: [CrewMember]
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top)
+
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
