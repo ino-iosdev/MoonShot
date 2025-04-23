54b18f41fe309f1f43eddb73f1a7fdb1e54cd7f7
ino yang popper <ino.yang.p@gmail.com>
2025-04-23 15:11:40 -0700

    challenge

---
+++ MoonShot/HorizontalScrollview.swift
@@ -0,0 +1,48 @@
+//
+//  HorizontalScrollview.swift
+//  MoonShot
+//
+//  Created by Ino Yang Popper on 4/23/25.
+//
+
+import SwiftUI
+
+
+struct HorizontalScrollview: View {
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HorizontalScrollview(mission: Mission, astronauts: <#[String : Astronaut]#>)
+}
--- MoonShot/MissionView.swift
+++ MoonShot/MissionView.swift
@@ -12,10 +12,10 @@ struct MissionView: View {
         let role: String
         let astronaut: Astronaut
     }
-    
-    let mission: Mission
-    let crew: [CrewMember]
 
+    let crew: [CrewMember]
+    let mission: Mission
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -25,16 +25,36 @@ struct MissionView: View {
                     .containerRelativeFrame(.horizontal) { width, axis in
                         width * 0.6
                     }
-                    .padding(.top)
+                
+                    .padding()
 
-                VStack(alignment: .leading) {
+                VStack(alignment: .center) {
+                    
+                    Text("Launch Date: \(mission.launchDate?.formatted(.dateTime.day().month().year()) ?? "N/A")")
+                        .font(.caption)
+                    
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundStyle(.lightBackground)
+                        .padding(.vertical)
+                    
                     Text("Mission Highlights")
                         .font(.title.bold())
                         .padding(.bottom, 5)
 
                     Text(mission.description)
+                    
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundStyle(.lightBackground)
+                        .padding(.vertical)
+                    
+                    Text("Crew")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
                 }
                 .padding(.horizontal)
+                
             }
             .padding(.bottom)
         }
@@ -42,11 +62,24 @@ struct MissionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground)
     }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
 }
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
 
-    return MissionView(mission: missions[0])
+    return MissionView(mission: missions[0], astronauts: astronauts)
         .preferredColorScheme(.dark)
 }
